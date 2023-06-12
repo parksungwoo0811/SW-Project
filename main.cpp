ec7d800f529b66a990b469a6188e48e6a77b8631
qkrgmlfud <107354117+huiryeong@users.noreply.github.com>
2023-06-12 09:59:27 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,12 @@ struct
     int x;
     int y;
 };
+bool isAttacking = false; // 플레이어의 공격 여부를 나타내는 변수
+    bool isCooling = false; // 플레이어의 쿨타임 여부를 나타내는 변수
+    sf::RectangleShape attackRectangle; // 공격에 사용할 직사각형
+    sf::Clock attackClock; // 공격 직사각형의 지속 시간을 측정하기 위한 시계
+    sf::Clock cooldownClock; // 쿨타임을 측정하기 위한 시계
+    const float cooldownDuration = 3.0f; // 쿨타임의 지속 시간
 
 
 vector<Texture> textures;
